05910e52e278cea1d052e774cb5ccfcc40de0534
Yudai Takada <t.yudai92@gmail.com>
2018-09-27 23:21:26 +0900

    Change link style.

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -5,6 +5,9 @@ Wordpress2016.overrideThemeStyles = () => ({
   'a.gatsby-resp-image-link': {
     boxShadow: 'none',
   },
+  'a': {
+    color: '#4C2600',
+  },
 })
 
 delete Wordpress2016.googleFonts
